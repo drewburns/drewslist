dee14316dae14cd11555e7c28ebbe0ac0ce85bfd
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2014-12-06 11:10:55 -0500

    Only need to fix the eidting

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,56 +6,70 @@ end
 
 
 get '/home' do
-	@posts = []
-	Post.where("category = ?", "home").each{|post| @posts << post}
-	erb :home
+	
+	@posts = Post.where("category = ?", "home")
+	erb :category
 end
 
 
 get '/sports' do
-		@posts = []
-	Post.where("category = ?", "sports").each{|post| @posts << post}
-	erb :sports
+	@posts = Post.where("category = ?", "sports")
+	erb :category
 end
 
 get '/computers' do
-		@posts = []
-	Post.where("category = ?", "computers").each{|post| @posts << post}
-	erb :computers
+	@posts =Post.where("category = ?", "computers")
+	erb :category
 end
 
 get '/sales' do
-		@posts = []
-	Post.where("category = ?", "sales").each{|post| @posts << post}
-	erb :sales
+	@posts =  Post.where("category = ?", "sales")
+	erb :category
 end
 
 get '/trades' do
-	@posts = []
-	Post.where("category = ?", "trades").each{|post| @posts << post}
-	erb :trades
+	@posts = Post.where("category = ?", "trades")
+	erb :category
 end
 
-get '/:post_id'
-	
-end
+
 
 get '/create' do
 	erb :create
 end	
 
 post '/create' do
-	Post.create(id: (Post.all.size) + 1,category: params[:category]  , title:  params[:title] , body: params[:body] , edit_url: (0...10).map{ ('a'..'z').to_a[rand(10)] }.join , author: params[:author])
-	erb :index
+	@post = Post.create(id: (Post.all.size) + 1,category: params[:category]  , title:  params[:title] , body: params[:body] , edit_url: (0...10).map{ ('a'..'z').to_a[rand(10)] }.join , author: params[:author])
+	erb :post
 end
 
 
-get '/:edit' do
-	@key = params[:edit_key]
+get '/edit/:key' do
+	@key = params[:key]
 	if Post.where("edit_url = ?", @key).size == 0
 		erb :error
 	else
+		@post = Post.where("edit_url = ?", @key)
+		@post = @post.first
 		erb :edit
 	end
-
 end
+
+post '/edit/:key' do
+	key = params[:key]
+	@post = Post.where("edit_url = ?", key )
+	@post = @post.first
+
+	@post.body = params[:body]
+	@post.update
+	@post.save
+	erb :post
+end
+
+get '/post/:id' do
+	@post = Post.where("id = ?", params[:id])
+	@post = @post.first
+	erb :post
+end
+
+
